2231c8d42ba323b707ffb5aa444de167925bcfba
zain0313233 <zain0313233@gmail.com>
2025-08-16 14:04:33 +0500

    today work

--- models/user.js
+++ models/user.js
@@ -6,7 +6,6 @@ const userSchema = new mongoose.Schema({
     required: true,
     trim: true
   },
-
   email: {
     type: String,
     required: true,
@@ -14,62 +13,32 @@ const userSchema = new mongoose.Schema({
     lowercase: true,
     trim: true
   },
-
   phone: {
     type: String,
     trim: true
   },
-
   country: {
     type: String,
     trim: true
   },
-
   address: {
     type: String,
     trim: true
   },
-
   dateOfBirth: {
     type: Date
   },
-
   password: {
     type: String,
     required: true,
     minlength: 6
   },
-
   role: {
     type: String,
     enum: ["student", "instructor", "admin", "teacher"],
     default: "student"
   },
   profileImageUrl: {
-    type: String, 
-    trim: true
-  },
-
-  bio: {
-    type: String,
-    trim: true
-  },
-
-  educationLevel: {
-    type: String,
-    trim: true
-  },
-  experience: {
-    type: Number
-  },
-
-  subjects: [
-    {
-      type: String,
-      trim: true
-    }
-  ],
-  learningGoals: {
     type: String,
     trim: true
   },
@@ -77,12 +46,33 @@ const userSchema = new mongoose.Schema({
     type: String,
     trim: true
   },
-
+  profile: {
+    bio: {
+      type: String,
+      trim: true
+    },
+    educationLevel: {
+      type: String,
+      trim: true
+    },
+    experience: {
+      type: Number
+    },
+    subjects: [
+      {
+        type: String,
+        trim: true
+      }
+    ],
+    learningGoals: {
+      type: String,
+      trim: true
+    }
+  },
   createdAt: {
     type: Date,
     default: Date.now
   },
-
   updatedAt: {
     type: Date,
     default: Date.now
